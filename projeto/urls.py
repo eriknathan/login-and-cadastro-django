8569832d50b6b4224401121ce0628aa854d4683f
erik-nathan <eriknathan.contato@gmail.com>
2023-04-30 13:07:30 -0300

    feat: criando a funão de cadastro e login

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home
+from app.views import home, create, store, painel, dologin, dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('create/', create),
+    path('painel/', painel),
+    path('store/', store),
+    path('dologin/', dologin),
+    path('dashboard/', dashboard),
 ]
